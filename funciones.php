cca213849c1ab13c679f884f36ebbcadde2ec25c
varelay27 <122463350+varelay27@users.noreply.github.com>
2023-01-11 16:19:20 -0600

    Concepto v1
    Primera versión funcional

--- funciones.php
+++ funciones.php
@@ -1,21 +1,40 @@
 <?php
 
+function revisar_dato ($var) {
+    if (empty($var)){
+        return 'NULL'; 
+    } elseif(preg_match("/[a-z]/i",$var)) {
+        return '\''.$var.'\'';
+    } else {
+        return $var; 
+    }
+}
+
 if (isset($_POST['enviar_meta'])) {
 
-    $SRC_SCHEMA_ID = $_POST['SRC_SCHEMA_ID'];
-    if (empty($SRC_SCHEMA_ID)){
-        $SRC_SCHEMA_ID = 'NULL'; 
-    } elseif(preg_match("/[a-z]/i",$SRC_SCHEMA_ID)) {
-        $SRC_SCHEMA_ID = "'$SRC_SCHEMA_ID'"; 
-    }
+    $SRC_SCHEMA_ID = revisar_dato($_POST['SRC_SCHEMA_ID']);
+    $OBJ_NAME = revisar_dato($_POST['OBJ_NAME']);
+    $ERROR_THRESHOLD = revisar_dato($_POST['ERROR_THRESHOLD']);
+    $LOAD_GRP = revisar_dato($_POST['LOAD_GRP']);
+    $LOAD_TYPE = revisar_dato($_POST['LOAD_TYPE']);
+    $OBJ_JOB_PARAMETERS = revisar_dato($_POST['OBJ_JOB_PARAMETERS']); 
+    $SELECT_STMT = revisar_dato($_POST['SELECT_STMT']); 
+    $FILTER_CONDITIONS = revisar_dato($_POST['FILTER_CONDITIONS']);
+    $ACTIVE_FLG = revisar_dato($_POST['ACTIVE_FLG']);
+    $INCR_COL_FILT1 = revisar_dato($_POST['INCR_COL_FILT1']);
+    $INCR_DAYS = revisar_dato($_POST['INCR_DAYS']);
+    $SPLIT_BY_COL = revisar_dato($_POST['SPLIT_BY_COL']);
+    $MAPPERS = revisar_dato($_POST['MAPPERS']);
+    $HARDDELETE = revisar_dato($_POST['HARDDELETE']);
+    $CREATED_DTTM = revisar_dato($_POST['CREATED_DTTM']);
+    $UPDATED_DTTM = revisar_dato($_POST['UPDATED_DTTM']);
+    $CREATED_BY = revisar_dato($_POST['CREATED_BY']);
+    $UPDATED_BY = revisar_dato($_POST['UPDATED_BY']); 
+    $APP_ID = revisar_dato($_POST['APP_ID']); 
+    $PARTITION_COL = revisar_dato($_POST['PARTITION_COL']);
+    $CONTAINER = revisar_dato($_POST['CONTAINER']);
 
-    $CONTAINER = $_POST['CONTAINER'];
-    if (empty($CONTAINER)){
-        $CONTAINER = 'NULL'; 
-    } elseif(preg_match("/[a-z]/i",$CONTAINER)) {
-        $CONTAINER = "'$CONTAINER'"; 
-    }
-    
+
     echo "INSERT INTO [dbo].[OBJ_METADATA] (
         SRC_SCHEMA_ID 
         ,OBJ_ID
@@ -40,28 +59,27 @@ if (isset($_POST['enviar_meta'])) {
         ,PARTITION_COL
         ,CONTAINER
         ) VALUES(
-            $SRC_SCHEMA_ID
+             $SRC_SCHEMA_ID
             ,(NEXT VALUE FOR [dbo].[OBJ_ID_SEQ])
-            ,'cur_tmp_opm_calendar1'
-            ,0
-            ,0
-            ,'F'
-            ,concat((select obj_id from dbo.OBJ_METADATA where SRC_SCHEMA_ID = 570002 and lower(obj_name) in ('cur_tmp_opm_calendario_turnos','cur_tmp_opm_calendar_exceptions')
-            )asd ORDER BY asd.OBJ_ID FOR XML PATH('') ), 1, 1, ''))),'|/curated/comex_manufacture/1.0/temps/tmp_opm_calendar1|249|0')
-            ,NULL
-            ,NULL
-            ,'Y'
-            ,NULL
-            ,NULL
-            ,NULL
-            ,NULL
-            ,'N'
-            ,getdate()
-            ,getdate()
-            ,'aluevano@ppg.com'
-            ,'aluevano@ppg.com'
-            ,NULL
-            ,NULL
+            ,$OBJ_NAME
+            ,$ERROR_THRESHOLD
+            ,$LOAD_GRP
+            ,$LOAD_TYPE
+            ,$OBJ_JOB_PARAMETERS
+            ,$SELECT_STMT
+            ,$FILTER_CONDITIONS
+            ,$ACTIVE_FLG
+            ,$INCR_COL_FILT1
+            ,$INCR_DAYS
+            ,$SPLIT_BY_COL
+            ,$MAPPERS
+            ,$HARDDELETE
+            ,$CREATED_DTTM
+            ,$UPDATED_DTTM
+            ,$CREATED_BY
+            ,$UPDATED_BY
+            ,$APP_ID
+            ,$PARTITION_COL
             ,$CONTAINER
             );
     ";
